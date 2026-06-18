f522dde064c8bf79dee44786e63e6ba803ca7da4
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-18 17:11:35 -0500

    Update Main.java

--- completed-examples/lesson-01-hello-world/Main.java
+++ completed-examples/lesson-01-hello-world/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola, Java!");
-        System.out.println("Estoy aprendiendo a programar.");
+        System.out.println("Hola me llamo Pepito, soy un programador");
     }
 }
