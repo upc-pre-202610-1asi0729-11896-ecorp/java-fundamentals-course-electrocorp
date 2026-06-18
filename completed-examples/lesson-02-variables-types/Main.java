ba7a8c8c31f748c013f4ee62484bae2b47f2b078
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-18 17:53:17 -0500

    Update Main.java

--- completed-examples/lesson-02-variables-types/Main.java
+++ completed-examples/lesson-02-variables-types/Main.java
@@ -1,13 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        String nombre = "Ana";
-        int edad = 15;
-        double nota = 17.5;
-        boolean inscrito = true;
 
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Nota: " + nota);
-        System.out.println("Esta inscrito: " + inscrito);
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    String nombre = "Pepito";
+	    int edad = 15;
+	    double nota = 17.5;
+	    boolean inscrito = true;
+	    
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Edad: " + edad);
+		System.out.println("Nota: " + nota);
+		System.out.println("Esta inscrito: " + inscrito);
+	}
 }
