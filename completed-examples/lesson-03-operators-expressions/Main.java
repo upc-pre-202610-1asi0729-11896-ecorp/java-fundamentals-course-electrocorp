a03a1ed236b7582dc9db55f205ee8587210712b6
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-18 18:53:02 -0500

    Update Main.java

--- completed-examples/lesson-03-operators-expressions/Main.java
+++ completed-examples/lesson-03-operators-expressions/Main.java
@@ -1,20 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        int numeroUno = 12;
-        int numeroDos = 4;
-        String nombre = "Ana";
-        String apellido = "Lopez";
 
-        int suma = numeroUno + numeroDos;
-        int resta = numeroUno - numeroDos;
-        int multiplicacion = numeroUno * numeroDos;
-        int division = numeroUno / numeroDos;
-        String nombreCompleto = nombre + " " + apellido;
-
-        System.out.println("Suma: " + suma);
-        System.out.println("Resta: " + resta);
-        System.out.println("Multiplicacion: " + multiplicacion);
-        System.out.println("Division: " + division);
-        System.out.println("Nombre completo: " + nombreCompleto);
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    String nombre = "Pepito";
+	    String apellido = "Perez";
+	    int notaUno = 12;
+	    int notaDos = 18;
+	    boolean limpiezaCasa = true;
+	    
+	    String nombreCompleto = nombre + " " + apellido;
+	    int promedio = (notaUno + notaDos)/2;
+	    String resultado = (promedio>=11) ? "Aprobado" : "Desaprobado";
+	    boolean salidaAmigos = (resultado == "Aprobado") && (limpiezaCasa=true);
+	    
+		System.out.println("Nombre Completo:" + nombreCompleto);
+		System.out.println("Su promedio es: " + promedio);
+		System.out.println("¿Salió aprobado?: " + resultado);		
+		System.out.println("¿Saldrá con sus amigos? " + salidaAmigos);		
+	}
 }
